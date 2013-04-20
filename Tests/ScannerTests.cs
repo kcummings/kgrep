dafbc9364f5f836e5e9547d3fb000332aafad6c9
Kevin Cummings <kevin.cummings@gmx.com>
2013-04-20 16:10:45 -0400

    Update ScannerTests to use ReplacerEngine, i.e. include in ReplacementFile.

--- Tests/ScannerTests.cs
+++ Tests/ScannerTests.cs
@@ -1,4 +1,5 @@
-﻿using kgrep;
+﻿using System.Collections.Generic;
+using kgrep;
 using NUnit.Framework;
 
 namespace Tests {
@@ -6,57 +7,73 @@ namespace Tests {
         [TestFixture]
         public class ScannerTests {
             [Test]
-            public void TestScannerForSingleToken() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("b\n", engine.ScanForTokens("abc", "a(b)c"));
+            public void TestScannerForEndingToken() {
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("bye bye (.*?)$", new List<string> { "today bye bye birdie" });
+                Assert.AreEqual("birdie\n", results);
+            }
+
+            [Test]
+            public void TestScannerNew() {
+                ReplacerEngine engine = new ReplacerEngine() {sw = new WriteToString()};
+                string newline = engine.SearchAndReplaceTokens("bye", new List<string> {"today bye bye birdie"});
+                Assert.AreEqual("bye\nbye\n", newline);
             }
 
             [Test]
             public void TestScannerForMultipleGroupsOnOneLine() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("ell\no\n", engine.ScanForTokens("hello world", "h(...)(.)"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("h(...)(.)", new List<string> { "hello world" });
+                Assert.AreEqual("ell\no\n", results);
             }
 
             [Test]
             public void TestScannerForNoMatch() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("", engine.ScanForTokens("abc", "def"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("abc", new List<string> { "def" });
+                Assert.AreEqual("", results);
             }
 
             [Test]
             public void TestScannerForSingleGroup() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("bc\n", engine.ScanForTokens("abc", "a(bc)"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("a(bc)", new List<string> { "abc" });
+                Assert.AreEqual("bc\n", results);
             }
 
             [Test]
             public void TestScannerForTwoGroups() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("bc\n89\n", engine.ScanForTokens("abc 89", "(..) ([0-9]+)"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("(..) ([0-9]+)", new List<string> { "abc 89" });
+                Assert.AreEqual("bc\n89\n", results);
             }
 
             [Test]
             public void TestScannerForMatchValue() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("b\n", engine.ScanForTokens("abc", "b"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("b", new List<string> { "abc" });
+                Assert.AreEqual("b\n", results);
             }
 
             [Test]
             public void TestScannerForSingleLetters() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("a\nb\nc\n \nd\n", engine.ScanForTokens("abc d", "."));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens(".", new List<string> { "abc d" });
+                Assert.AreEqual("a\nb\nc\n \nd\n", results);
             }
 
             [Test]
             public void TestScannerForSingleDigits() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("8\n5\n1\n2\n3\n", engine.ScanForTokens("85 dollars 123 lost", "[0-9]"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("[0-9]", new List<string> { "85 dollars 123 lost" });
+                Assert.AreEqual("8\n5\n1\n2\n3\n", results);
             }
 
             [Test]
             public void TestScannerForNumbers() {
-                ScannerEngine engine = new ScannerEngine();
-                Assert.AreEqual("85\n123\n", engine.ScanForTokens("85 dollars 123 lost", "[0-9]+"));
+                ReplacerEngine engine = new ReplacerEngine() { sw = new WriteToString() };
+                string results = engine.SearchAndReplaceTokens("[0-9]+", new List<string> { "85 dollars 123 lost" });
+                Assert.AreEqual("85\n123\n", results);
             }
         }
     }
