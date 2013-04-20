48478253794fb34ad0d87d60c27c78dcfaf1b0ef
Kevin Cummings <kevin.cummings@gmx.com>
2013-04-20 14:46:39 -0400

    Explicitly check for "scope=all" in replacement file

--- src/ReplacementFile.cs
+++ src/ReplacementFile.cs
@@ -10,8 +10,8 @@ namespace kgrep
         public List<Replacement> ReplacementList = new List<Replacement>();
         private bool _ScopeAll = true;
         public bool ScopeAll { get { return _ScopeAll; } }
-        private String COMMENT = "#";
-        private String DELIM = "~";
+        private String _comment = "#";
+        private String _delim = "~";
         private IHandleInput sr;
 
         public ReplacementFile(String filename) {
@@ -28,37 +28,33 @@ namespace kgrep
                 line = line.Trim();
 
                 // Remove comment lines.
-                if (line == String.Empty || line.StartsWith(COMMENT)) {    
+                if (line == String.Empty || line.StartsWith(_comment)) {    
                     continue;
                 }
 
-                if (line.ToLower().StartsWith("comment=")) {
-                    COMMENT = line.Substring("comment=".Length, 1);
-                    continue;
-                }
-
-                if (line.ToLower().StartsWith("delim=")) {
-                    DELIM = line.Substring("delim=".Length, 1);
-                    continue;
-                }
-                
-                // Once true, it's true for the remaining replacements.
-                if (line.ToLower().StartsWith("scope=first")) {
+                if (line.ToLower().StartsWith("comment=")) 
+                    _comment = line.Substring("comment=".Length, 1);
+                else if (line.ToLower().StartsWith("delim=")) 
+                    _delim = line.Substring("delim=".Length, 1);
+                else if (line.ToLower().StartsWith("scope=first"))  // Once true, it's true for the remaining replacements.
                     _ScopeAll = false;
-                    continue;
-                }
+                else if (line.ToLower().StartsWith("scope=all"))  
+                    _ScopeAll = true;
+                else {
+                    // Remove any trailing comments.
+                    int i = line.IndexOf(_comment);
+                    if (i >= 0)
+                        line = line.Remove(i);
 
-                // Remove any trailing comments.
-                int i = line.IndexOf(COMMENT);
-                if (i >= 0)
-                    line = line.Remove(i);
-
-                String[] parts = line.Split(DELIM.ToCharArray(),4);
-                if (parts.Length == 2) {  // just a~b pattern
-                    replacementList.Add(new Replacement(parts[0], parts[1]));
-                }
-                if (parts.Length == 3) {    // anchored a~b pattern
-                    replacementList.Add(new Replacement(parts[0], parts[1], parts[2]));
+                    String[] parts = line.Split(_delim.ToCharArray(), 4);
+                    if (parts.Length == 2) {
+                        // just a~b pattern
+                        replacementList.Add(new Replacement(parts[0], parts[1]));
+                    }
+                    if (parts.Length == 3) {
+                        // anchored a~b pattern
+                        replacementList.Add(new Replacement(parts[0], parts[1], parts[2]));
+                    }
                 }
             }
             sr.Close();
