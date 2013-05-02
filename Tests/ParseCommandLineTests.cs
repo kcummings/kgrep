b701e01ae2db779b534b422740aba74f49941681
Kevin Cummings <kevin.cummings@gmx.com>
2013-05-02 19:42:48 -0400

    Refactor ParseCommandLine Tests to use [TestCase] attribute

--- Tests/ParseCommandLineTests.cs
+++ Tests/ParseCommandLineTests.cs
@@ -10,60 +10,52 @@ namespace Tests {
     [TestFixture]
     public class ParseCommandLineTests {
 
-        [Test]
-        // kgrep frompattern frompattern2
-        public void TestScanAndReplacement() {
-            string[] args = new String[] { "a~c", "abc" };
+        [TestCase("a~c", "abc", "a~c")]
+        [TestCase("a~c", "a~bc", "a~c")]   
+        public void TestForTokenAndReplacementFileArguments(string token, string repfile, string expected) {
+            string[] args = new String[] { token, repfile };
             ParseCommandLine cmd = new ParseCommandLine(args);
-            Assert.AreEqual("a~c", cmd.ReplacementFileName);
-            Assert.AreEqual(1, cmd.InputSourceNames.Count);
-        }
-
-        [Test]
-        // kgrep frompattern frompattern2
-        public void TestTwoScans() {
-            string[] args = new String[] { "a~c", "a~bc" };
-            ParseCommandLine cmd = new ParseCommandLine(args);
-            Assert.AreEqual("a~c", cmd.ReplacementFileName);
+            Assert.AreEqual(expected, cmd.ReplacementFileName);
             Assert.AreEqual(1, cmd.InputSourceNames.Count);
         }
 
         [Test]
         // kgrep frompattern filename1
-        public void TestSimpleScanSingleFile() {
+        public void TestForTokenAndFilenameArguments() {
             string[] args = new String[] { "abc", "file1.txt" };
             ParseCommandLine cmd = new ParseCommandLine(args);
             Assert.AreEqual(1, cmd.InputSourceNames.Count);
             Assert.AreEqual("file1.txt", cmd.InputSourceNames[0]);
         }
 
-        [Test]
+        [TestCase("abc","file1.txt","file2.txt", "file1.txt", "file2.txt")]
+        [TestCase("a~c; b~d", "file1.txt", "file2.txt", "file1.txt","file2.txt")]
+        [TestCase("#comment; a~c; b~d", "file1.txt", "file2.txt", "file1.txt", "file2.txt")]
         // kgrep frompattern filename1 filename2
-        public void TestSimpleScanTwoFiles() {
-            string[] args = new String[] { "abc", "file1.txt", "file2.txt" };
+        public void TestForStackedTokensAndTwoSourceFilesArguments(string token, string file1, string file2, string expected1, string expected2) {
+            string[] args = new String[] { token, file1, file2 };
             ParseCommandLine cmd = new ParseCommandLine(args);
             Assert.AreEqual(2, cmd.InputSourceNames.Count);
-            Assert.AreEqual("file1.txt", cmd.InputSourceNames[0]);
-            Assert.AreEqual("file2.txt", cmd.InputSourceNames[1]);
+            Assert.AreEqual(expected1, cmd.InputSourceNames[0]);
+            Assert.AreEqual(expected2, cmd.InputSourceNames[1]);
         }
 
         [Test]
-        // kgrep frompattern filename1 filename2
-        public void TestSimpleReplacementTwoFiles() {
-            string[] args = new String[] { "a~c; b~d", "file1.txt", "file2.txt" };
-            ParseCommandLine cmd = new ParseCommandLine(args);
-            Assert.AreEqual("a~c; b~d", cmd.ReplacementFileName);
-            Assert.AreEqual(2, cmd.InputSourceNames.Count);
-            Assert.AreEqual("file1.txt", cmd.InputSourceNames[0]);
-            Assert.AreEqual("file2.txt", cmd.InputSourceNames[1]);
-        }
-        [Test]
         // cat filename|kgrep replacementFilename
-        public void TestReplacementFileScan() {
+        public void TestForStdinAsInputSourceArguments() {
             string[] args = new String[] { "hi~bye" };
             ParseCommandLine cmd = new ParseCommandLine(args);
             Assert.AreEqual("hi~bye", cmd.ReplacementFileName);
             Assert.AreEqual(1, cmd.InputSourceNames.Count);
+            Assert.AreEqual(cmd.STDIN, cmd.InputSourceNames[0]);
+        }
+
+        [Test]
+        public void TestForNoInputSourceArguments() {
+            string[] args = new String[] { "" };
+            ParseCommandLine cmd = new ParseCommandLine(args);
+            Assert.AreEqual("", cmd.ReplacementFileName);
+            Assert.AreEqual(1, cmd.InputSourceNames.Count);  // empty string
             Assert.AreEqual(cmd.STDIN, cmd.InputSourceNames[0]);
         }
     }
