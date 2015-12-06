6506017f160dae6e78cce4273e44366319704b0e
kcummings <kevin.cummings@gmx.com>
2015-12-06 09:47:57 -0500

    Refactor to separate replacing using a full line vs. only the matched sections.
    Defaults to full line mode woth no way to override that setting yet.

--- src/ReplaceTokens.cs
+++ src/ReplaceTokens.cs
@@ -9,6 +9,7 @@ namespace kgrep {
     public class ReplaceTokens : IFileAction {
         public IHandleOutput sw = new WriteStdout();
         private readonly Pickup _pickup;
+        private bool verbose = true;
 
         public ReplaceTokens() {
             _pickup = new Pickup();
@@ -27,6 +28,8 @@ namespace kgrep {
 
         private void ApplyCommandsToFile(ParseCommandFile commandFile, string filename) {
             IHandleInput sr = (new ReadFileFactory()).GetSource((filename));
+            verbose = commandFile.OutputAllLines;
+
             string line;
             while ((line = sr.ReadLine()) != null) {
                 string alteredLine = ApplyCommandsToLine(line, commandFile.CommandList);
@@ -54,16 +57,38 @@ namespace kgrep {
 
         private string ApplySingleCommand(string line, Command command) {
             _pickup.CollectAllPickupsInLine(line, command);
-            line = ReplaceIt(command.SubjectRegex, line, _pickup.ReplacePickupsWithStoredValue(command.ReplacementString));
+            if (verbose)
+                line = ReplaceFullLine(command.SubjectRegex, line, _pickup.ReplacePickupsWithStoredValue(command.ReplacementString));
+            else {
+                line = ReplaceMatched(command.SubjectRegex, line, _pickup.ReplacePickupsWithStoredValue(command.ReplacementString),command.OFS);
+            }
             return line;
         }
 
-        private string ReplaceIt(Regex re, string source, string target) {
-            int count = re.Matches(source).Count;
-            if (count>0) {
-                return re.Replace(source, target);
+        private string ReplaceMatched(Regex re, string source, string target, string FS) {
+             List<string> sb = new List<string>();
+             Match m = re.Match(source);
+
+            // Only return submatches if found, otherwise return any matches.
+            while (m.Success) {
+                int[] gnums = re.GetGroupNumbers();
+                if (gnums.Length > 1) {  // does source contain any captured text?
+                    for (int i = 1; i < gnums.Length; i++) {
+                        string capturedText = m.Groups[gnums[i]].Value;
+                        string newText = re.Replace(capturedText, target);
+                        sb.Add(newText);
+                    }
+                } else {
+                    // Only include the substring that was matched.
+                    sb.Add(m.Value);
+                }
+                m = m.NextMatch();
             }
-            return source;
+            return String.Join(FS, sb.ToArray()); 
+        }
+
+        private string ReplaceFullLine(Regex re, string source, string target) {
+            return re.Replace(source, target);
         }
 
         private bool isCandidateForReplacement(string line, Command command) {
